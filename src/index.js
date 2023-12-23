4985de9ae8b3857b0d625f26696e2f1506e6b5df
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-12-23 12:41:34 +0100

    Quickfix for main index.js imports

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
-import { theme } from "./theme";
+import { theme } from "./theme.js";
 import { Normalize } from "styled-normalize";
-import { GlobalStyleComponent } from './styled';
+import { GlobalStyleComponent } from './styled.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
