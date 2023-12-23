6502136cc5ec6621a4860c61795ac9fd1be1e6c7
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-12-23 11:27:32 +0100

    Initial theme file

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    colors: {
+        white: "#FFFFFF",
+        gold: "#FFD700",
+        goldenGrass: "#DAA520",
+
+    },
+
+    breakpoints: {
+        mobile: "767px",
+    },
+};
