26412f6f836bb6a89b1618a514d2c48b72a13d5f
Maciej Lenartowicz <lenarmathias@gmail.com>
2024-01-07 16:38:13 +0100

    Initial creation of NavigationBar component

---
+++ src/core/NavigationBar/index.js
@@ -0,0 +1,17 @@
+import {
+    NavigationBarContainer,
+    NavigationBarWrapper,
+    NavigationBarHeading
+} from "./styled";
+
+const NavigationBar = () => (
+    <NavigationBarContainer>
+        <NavigationBarWrapper>
+            <NavigationBarHeading>
+                BMI Calculator
+            </NavigationBarHeading>
+        </NavigationBarWrapper>
+    </NavigationBarContainer>
+);
+
+export default NavigationBar;
---
+++ src/core/NavigationBar/styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const NavigationBarContainer = styled.div`
+    background: linear-gradient(
+        ${({ theme }) => theme.colors.gold},
+        ${({ theme }) => theme.colors.goldenGrass}
+        );
+    width: 100vw;
+    height: 40px;
+    position: fixed;
+    top: 0;
+    left: 0;
+`;
+
+export const NavigationBarWrapper = styled.div`
+    margin: 0 auto;
+    width: 800px;
+    max-width: 90%;
+`;
+
+export const NavigationBarHeading = styled.h1`
+    margin: 0;
+    font-size: 26px;
+`;
